857f5ba16a204f7aed6fd0c5b76a1cede5934cae
Yi Guo <guoyi0328@gmail.com>
2022-04-23 06:02:54 -0700

    Added quest panels. Display functioning.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,81 @@
 import React from "react";
 import "./App.css";
+import { db } from "./init-fb";
 import { logIn, logOut } from "./auth";
-import { fsAddDoc, fsGetDocs } from "./fb-demo";
+// import { fsAddDoc, fsGetDocs } from "./fb-demo";
+import {
+    collection,
+    query,
+    where,
+    doc,
+    addDoc,
+    getDocs,
+} from "firebase/firestore";
+import { delay } from "q";
+
+class Quest extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quest: props.quest,
+        };
+    }
+
+    render() {
+        return <p>{this.state.quest.title}</p>;
+    }
+}
+
+class QuestPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: props.title,
+            type: props.type,
+            quests: [],
+        };
+    }
+
+    componentDidMount() {
+        this.fetchQuest();
+    }
+
+    async fetchQuest() {
+        const q = query(
+            collection(db, "active"),
+            where("type", "==", this.state.type)
+        );
+
+        const querySnapshot = await getDocs(q);
+
+        const quests = [];
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            const quest = doc.data();
+            quest.id = doc.id;
+            quests.push(quest);
+        });
+
+        this.setState({
+            quests: quests,
+        });
+    }
+
+    render() {
+        console.log("QuestPanel updated:", this.state);
+        const listItems = this.state.quests.map((quest) => (
+            <li key={quest.id}>
+                <Quest quest={quest} />
+            </li>
+        ));
+        return (
+            <div className="QuestPanel">
+                <h3>{this.state.title}</h3>
+                <ol>{listItems}</ol>
+            </div>
+        );
+    }
+}
 
 class App extends React.Component {
     constructor(props) {
@@ -45,26 +119,10 @@ class App extends React.Component {
                     <header className="App-header">
                         <h1>Hello {this.state.user.displayName}!</h1>
                         <button onClick={this.handleLogOut}>log out</button>
-                        <h3>主线任务</h3>
-                        <ol>
-                            <li>主线任务1</li>
-                            <li>主线任务2</li>
-                        </ol>
-                        <h3>支线任务</h3>
-                        <ol>
-                            <li>支线任务1</li>
-                            <li>支线任务2</li>
-                        </ol>
-                        <h3>每日任务</h3>
-                        <ol>
-                            <li>每日任务1</li>
-                            <li>每日任务2</li>
-                        </ol>
-                        <h3>可选任务</h3>
-                        <ol>
-                            <li>可选任务1</li>
-                            <li>可选任务2</li>
-                        </ol>
+                        <QuestPanel title="主线任务" type="main" />
+                        <QuestPanel title="支线任务" type="side" />
+                        <QuestPanel title="每日任务" type="daily" />
+                        <QuestPanel title="可选任务" type="optional" />
                     </header>
                 </div>
             );
