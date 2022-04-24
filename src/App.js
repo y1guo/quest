def1577fb499f1257aeb23e4f228645cae11d6ea
Yi Guo <guoyi0328@gmail.com>
2022-04-23 17:12:12 -0700

    Added quest panels. fixed app wide quest update, and the feature cancel editting.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { db } from "./init-fb";
 import { logIn, logOut } from "./auth";
@@ -13,22 +13,13 @@ import {
     Timestamp,
 } from "firebase/firestore";
 
-class NewQuest extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            type: props.type,
-        };
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    async handleClick() {
+function NewQuest(props) {
+    async function handleClick() {
         const currentTime = Timestamp.now();
         try {
             const docRef = await addDoc(collection(db, "active"), {
                 id: null,
-                type: this.state.type,
+                type: props.type,
                 title: "",
                 description: "",
                 note: "",
@@ -44,85 +35,92 @@ class NewQuest extends React.Component {
         }
     }
 
-    render() {
-        return (
-            <div className="newQuest">
-                <button onClick={this.handleClick}>+</button>
-            </div>
-        );
-    }
+    return (
+        <div className="newQuest">
+            <button onClick={handleClick}>+</button>
+        </div>
+    );
 }
 
-class Quest extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            quest: props.quest,
-            questsId: props.questsId,
-            modify: false,
-        };
+function Quest(props) {
+    const [quest, setQuest] = useState(questCopy(props.quest));
+    const [activeQuest, setActiveQuest] = useState(
+        questListCopy(props.activeQuest)
+    );
+    const [modify, setModify] = useState(false);
 
-        this.handleModify = this.handleModify.bind(this);
-        this.handleSave = this.handleSave.bind(this);
-        this.handleCancel = this.handleCancel.bind(this);
+    useEffect(() => {
+        setQuest(questCopy(props.quest));
+    }, [props.quest]);
+
+    useEffect(() => {
+        setActiveQuest(questListCopy(props.activeQuest));
+    }, [props.activeQuest]);
+
+    function handleModify() {
+        setModify(true);
     }
 
-    handleModify() {
-        this.setState({
-            modify: true,
-        });
-    }
-
-    async handleSave() {
-        const quest = this.state.quest;
+    async function handleSave() {
         quest.dateModified = Timestamp.now();
-        try {
-            await setDoc(doc(db, "active", quest.id), quest);
-            this.setState({
-                modify: false,
-            });
-            console.log("Document saved with ID: ", quest.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
+
+        await setDoc(doc(db, "active", quest.id), quest);
+        setModify(false);
+        console.log("Document saved with ID: ", quest.id);
     }
 
-    handleCancel() {
-        this.props.reload();
+    function handleCancel() {
+        setModify(false);
+        setQuest(questCopy(props.quest));
     }
 
-    handleSubmit(event) {
+    function handleSubmit(event) {
         event.preventDefault();
     }
 
-    textarea(key) {
+    function questCopy(q) {
+        return {
+            id: q.id,
+            type: q.type,
+            title: q.title,
+            description: q.description,
+            note: q.note,
+            dateAdded: q.dateAdded,
+            dateModified: q.dateModified,
+            dateActive: q.dateActive,
+            dateExpire: q.dateExpire,
+            dependency: [...q.dependency],
+        };
+    }
+
+    function questListCopy(questList) {
+        return questList.map((q) => questCopy(q));
+    }
+
+    function textarea(key) {
         const handleChange = (event) => {
-            const quest = this.state.quest;
-            quest[key] = event.target.value;
-            this.setState(quest);
+            const newQuest = questCopy(quest);
+            newQuest[key] = event.target.value;
+            setQuest(newQuest);
         };
         return (
             <textarea
                 type="text"
                 name={key}
-                value={this.state.quest[key]}
+                value={quest[key]}
                 onChange={handleChange}
             ></textarea>
         );
     }
 
-    select(key) {
+    function select(key) {
         const handleChange = (event) => {
-            const quest = this.state.quest;
-            quest[key] = event.target.value;
-            this.setState(quest);
+            const newQuest = questCopy(quest);
+            newQuest[key] = event.target.value;
+            setQuest(newQuest);
         };
         return (
-            <select
-                multiple={true}
-                value={this.state.quest[key]}
-                onChange={handleChange}
-            >
+            <select multiple={true} value={quest[key]} onChange={handleChange}>
                 <option value="grapefruit">Grapefruit</option>
                 <option value="lime">Lime</option>
                 <option value="coconut">Coconut</option>
@@ -131,183 +129,142 @@ class Quest extends React.Component {
         );
     }
 
-    render() {
-        if (this.state.modify) {
-            return (
-                <div className="quest">
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            Title:
-                            {this.textarea("title")}
-                        </label>
-                        <label>
-                            Description:
-                            {this.textarea("description")}
-                        </label>
-                        <label>
-                            Dependency:
-                            {this.select("dependency")}
-                        </label>
-                    </form>
-                    <button onClick={this.handleCancel}>cancel</button>
-                    <button onClick={this.handleSave}>save</button>
-                </div>
-            );
-        } else {
-            return (
-                <div className="quest">
-                    <h5>{this.state.quest.title}</h5>
-                    <p>{this.state.quest.description}</p>
-                    <p>Depending on:{this.state.quest.dependency}</p>
-                    <p>
-                        {this.state.quest.dateModified
-                            .toDate()
-                            .toLocaleString()}
-                    </p>
-                    <button onClick={this.handleModify}>mod</button>
-                </div>
-            );
-        }
-    }
-}
-
-class QuestPanel extends React.Component {
-    render() {
-        const questsId = this.props.quests.map((quest) => quest.id);
-        const listItems = this.props.quests
-            .filter((quest) => {
-                return quest.type === this.props.type;
-            })
-            .map((quest) => (
-                <li key={quest.id}>
-                    <Quest
-                        quest={quest}
-                        reload={this.props.reload}
-                        questsId={questsId}
-                    />
-                </li>
-            ));
-        // console.log(this.state.type, listItems);
+    if (modify) {
         return (
-            <div className="QuestPanel">
-                <h3>{this.props.title}</h3>
-                <ol>{listItems}</ol>
-                <NewQuest type={this.props.type} />
+            <div className="quest">
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Title:
+                        {textarea("title")}
+                    </label>
+                    <label>
+                        Description:
+                        {textarea("description")}
+                    </label>
+                    <label>
+                        Dependency:
+                        {select("dependency")}
+                    </label>
+                </form>
+                <button onClick={handleCancel}>cancel</button>
+                <button onClick={handleSave}>save</button>
+            </div>
+        );
+    } else {
+        return (
+            <div className="quest">
+                <h5>{quest.title}</h5>
+                <p>{quest.description}</p>
+                <p>Depending on:{quest.dependency}</p>
+                <p>{quest.dateModified.toDate().toLocaleString()}</p>
+                <button onClick={handleModify}>mod</button>
             </div>
         );
     }
 }
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: null,
-            quests: [],
-            unsubscribe: null,
-            updatedTime: Timestamp.now(),
-        };
+function QuestPanel(props) {
+    const listItems = props.quests
+        .filter((quest) => {
+            return quest.type === props.type;
+        })
+        .map((quest) => (
+            <li key={quest.id}>
+                <Quest quest={quest} activeQuest={props.quests} />
+            </li>
+        ));
+    // console.log(this.state.type, listItems);
+    return (
+        <div className="QuestPanel">
+            <h3>{props.title}</h3>
+            <ol>{listItems}</ol>
+            <NewQuest type={props.type} />
+        </div>
+    );
+}
 
-        this.handleLogIn = this.handleLogIn.bind(this);
-        this.handleLogOut = this.handleLogOut.bind(this);
-        this.fetchQuest = this.fetchQuest.bind(this);
-    }
+function App() {
+    const [user, setUser] = useState(null);
+    const [quests, setQuests] = useState([]);
+    const [updatedTime, setUpdateTime] = useState(Timestamp.now());
+    var unsubscribe = null;
 
-    handleLogIn() {
+    function handleLogIn() {
         logIn((user) => {
-            this.setState({
-                user: user,
-            });
-            this.fetchQuest();
+            setUser(user);
+            fetchQuest();
         });
     }
 
-    handleLogOut() {
-        this.state.unsubscribe();
+    function handleLogOut() {
+        unsubscribe();
         logOut(() => {
-            this.setState({
-                user: null,
-            });
+            setUser(null);
         });
     }
 
-    async fetchQuest() {
-        if (this.state.unsubscribe) {
-            this.state.unsubscribe();
+    function fetchQuest() {
+        if (unsubscribe) {
+            unsubscribe();
         }
 
-        try {
-            const q = query(collection(db, "active"));
-            const unsubscribe = onSnapshot(q, (querySnapshot) => {
-                const quests = [];
-                querySnapshot.forEach((doc) => {
-                    // doc.data() is never undefined for query doc snapshots
-                    const quest = doc.data();
-                    quest.id = doc.id;
-                    quests.push(quest);
-                });
-                this.setState({
-                    quests: [...quests],
-                });
+        const q = query(collection(db, "active"));
+        unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const newQuests = [];
+            console.log("in onsnapshot");
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                const quest = doc.data();
+                quest.id = doc.id;
+                newQuests.push(quest);
             });
+            setQuests([...newQuests]);
+        });
 
-            this.setState({
-                unsubscribe: unsubscribe,
-                updatedTime: Timestamp.now(),
-            });
+        setUpdateTime(Timestamp.now());
 
-            console.log("reloaded");
-        } catch (e) {
-            console.error("Error fetchQuest! ", e);
-        }
+        console.log("reloaded");
     }
 
-    render() {
-        if (!this.state.user) {
-            return (
-                <div className="App">
-                    <header className="App-header">
-                        <button onClick={this.handleLogIn}>log in</button>
-                    </header>
-                </div>
-            );
-        } else {
-            return (
-                <div className="App">
-                    <header className="App-header">
-                        <p>
-                            Last Update:{" "}
-                            {this.state.updatedTime.toDate().toLocaleString()}
-                        </p>
-                        <button onClick={this.handleLogOut}>log out</button>
-                        <QuestPanel
-                            title="main quests"
-                            type="main"
-                            quests={this.state.quests}
-                            reload={this.fetchQuest}
-                        />
-                        <QuestPanel
-                            title="side quests"
-                            type="side"
-                            quests={this.state.quests}
-                            reload={this.fetchQuest}
-                        />
-                        <QuestPanel
-                            title="daily quests"
-                            type="daily"
-                            quests={this.state.quests}
-                            reload={this.fetchQuest}
-                        />
-                        <QuestPanel
-                            title="optional quests"
-                            type="optional"
-                            quests={this.state.quests}
-                            reload={this.fetchQuest}
-                        />
-                    </header>
-                </div>
-            );
-        }
+    console.log("unsubscribe: ", unsubscribe);
+
+    if (!user) {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <button onClick={handleLogIn}>log in</button>
+                </header>
+            </div>
+        );
+    } else {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Last Update: {updatedTime.toDate().toLocaleString()}</p>
+                    <button onClick={handleLogOut}>log out</button>
+                    <QuestPanel
+                        title="main quests"
+                        type="main"
+                        quests={quests}
+                    />
+                    <QuestPanel
+                        title="side quests"
+                        type="side"
+                        quests={quests}
+                    />
+                    <QuestPanel
+                        title="daily quests"
+                        type="daily"
+                        quests={quests}
+                    />
+                    <QuestPanel
+                        title="optional quests"
+                        type="optional"
+                        quests={quests}
+                    />
+                </header>
+            </div>
+        );
     }
 }
 
